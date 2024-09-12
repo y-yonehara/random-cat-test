a4bc4efb4a1333434a8bada2054c6ec653580c61
y-yonehara <yonehara.yuta@probizmo.co.jp>
2024-09-12 14:10:23 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,8 @@ const IndexPage: NextPage<Props> = ({ initialImageUrl }) => {
   return (
     <div className={styles.page}>
       <button onClick={handleClick} className={styles.button}>
-        他のにゃんこも見る
+{/*         他のにゃんこも見る */}
+            One more cat!
       </button>
       <div className={styles.frame}>
         {loading || <img src={imageUrl} className={styles.img} />}
